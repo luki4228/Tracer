865a8c5c089f8ba55fb1b7ed234eb4960e3b875e
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-03 15:09:14 +0300

    Interface and Class

---
+++ Tracer/Tracer/TraceResult.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace Tracer
+{
+    [DataContract]
+    public class TraceResult
+    {
+        private ConcurrentDictionary<int, ThreadTracer> threadResults;
+
+        internal ConcurrentDictionary<int, ThreadTracer> ThreadTracer
+        {
+            get
+            {
+                if (threadResults == null)
+                {
+                    threadResults = new ConcurrentDictionary<int, ThreadTracer>();
+                }
+                return threadResults;
+            }
+        }
+
+        [DataMember(Name = "threads")]
+        public List<ThreadTracer> ThreadsList
+        {
+            get
+            {
+                return new List<ThreadTracer>(ThreadTracer.Values).OrderBy(item => item.ThreadId).ToList();
+            }
+
+            private set { }
+        }
+
+        internal TraceResult() { }
+
+        internal ThreadTracer AddThreadTracer(int id, ThreadTracer value)
+        {
+            if (ThreadTracer.TryAdd(id, value))
+            {
+                return GetThreadTracer(id);
+            }
+            return null;
+        }
+
+        internal ThreadTracer GetThreadTracer(int id)
+        {
+            ThreadTracer threadTracer;
+            if (ThreadTracer.TryGetValue(id, out threadTracer))
+            {
+                return threadTracer;
+            }
+            return null;
+        }
+
+
+    }
+}
---
+++ Tracer/Tracer/Tracer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class Tracer : ITracer
+    {
+        private TraceResult traceResult;
+        public TraceResult GetTraceResult()
+        {
+            return traceResult;
+        }
+
+        public void StartTrace()
+        {
+            MethodBase methodBase = new StackTrace().GetFrame(1).GetMethod();
+            string methodName = methodBase.Name;
+            string className = methodBase.ReflectedType.Name;
+            MethodTracer methodTracer = new MethodTracer(methodName, className);
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            ThreadTracer currentThreadTracer = traceResult.GetThreadTracer(threadId);
+            if (currentThreadTracer == null)
+            {
+                currentThreadTracer = new ThreadTracer(threadId);
+                currentThreadTracer = traceResult.AddThreadTracer(threadId, currentThreadTracer);
+            }
+            currentThreadTracer.StartTrace(methodTracer);
+        }
+
+        public void StopTrace()
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            ThreadTracer threadTracer = traceResult.GetThreadTracer(threadId);
+            if (threadTracer != null)
+            {
+                threadTracer.StopTrace();
+            }
+            else
+            {
+                throw new InvalidOperationException("There are no one method to stop tracing");
+            }
+        }
+
+        public Tracer()
+        {
+            traceResult = new TraceResult();
+        }
+
+    }
+}
