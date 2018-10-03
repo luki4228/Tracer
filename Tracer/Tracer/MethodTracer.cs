80b8b5b5d5655910eb34acbaf933d2731c4fa59a
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-03 16:30:02 +0300

    added MethodTracer and ThreadTracer

---
+++ Tracer/Tracer/MethodTracer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace Tracer
+{
+    [DataContract]
+    public class MethodTracer
+    {
+        private List<MethodTracer> innerMethods;
+        private string name;
+        private string className;
+        private Stopwatch stopwatch = new Stopwatch();
+
+        [DataMember(Name = "name", Order = 0)]
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set { }
+        }
+
+        [DataMember(Name = "class", Order = 1)]
+        public string ClassName
+        {
+            get
+            {
+                return className;
+            }
+            private set { }
+        }
+
+        public long Time
+        {
+            get
+            {
+                if (!stopwatch.IsRunning)
+                {
+                    return stopwatch.ElapsedMilliseconds;
+                }
+                else
+                {
+                    throw new Exception("Stopwatch is running");
+                }
+            }
+        }
+
+        [DataMember(Name = "time", Order = 2)]
+        internal string TimeFormated
+        {
+            get
+            {
+                return Time.ToString() + "ms";
+            }
+
+            private set { }
+        }
+
+        [DataMember(Name = "methods", Order = 3)]
+        public List<MethodTracer> InnerMethods
+        {
+            get
+            {
+                if (innerMethods == null)
+                {
+                    innerMethods = new List<MethodTracer>();
+                }
+                return innerMethods;
+            }
+        }
+
+        internal void AddInnerMethod(MethodTracer method)
+        {
+            InnerMethods.Add(method);
+        }
+
+        internal MethodTracer(string methodName, string methodClassName)
+        {
+            name = methodName;
+            className = methodClassName;
+            stopwatch = new Stopwatch();
+        }
+
+        internal void StartTrace()
+        {
+            stopwatch.Start();
+        }
+
+        internal void StopTrace()
+        {
+            stopwatch.Stop();
+        }
+    }
+}
---
+++ Tracer/Tracer/ThreadTracer.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Runtime.Serialization;
+
+namespace Tracer
+{
+    [DataContract]
+    public class ThreadTracer
+    {
+        private int threadId;
+        private Stack<MethodTracer> methodsInThread;
+        private List<MethodTracer> tracedMethods;
+
+        [DataMember(Name = "id", Order = 0)]
+        internal int ThreadId { get; private set; }
+
+        public long Time
+        {
+            get
+            {
+                long t = 0;
+                tracedMethods.ForEach((item) => t += item.Time);
+                return t;
+            }
+        }
+
+        [DataMember(Name = "time", Order = 1)]
+        internal string TimeFormated
+        {
+            get
+            {
+                return Time.ToString() + "ms";
+            }
+
+            private set { }
+        }
+
+        internal Stack<MethodTracer> MethodsInThread
+        {
+            get
+            {
+                if (methodsInThread == null)
+                {
+                    methodsInThread = new Stack<MethodTracer>();
+                }
+                return methodsInThread;
+            }
+
+            private set { }
+        }
+
+        [DataMember(Name = "methods", Order = 2)]
+        internal List<MethodTracer> TracedMethods
+        {
+            get
+            {
+                if (tracedMethods == null)
+                {
+                    tracedMethods = new List<MethodTracer>();
+                }
+                return tracedMethods;
+            }
+        }
+
+        public List<MethodTracer> InnerMethods
+        {
+            get
+            {
+                return new List<MethodTracer>(TracedMethods);
+            }
+            private set { }
+        }
+
+        private void AddMethod(MethodTracer method)
+        {
+            if (MethodsInThread.Count > 0)
+            {
+                MethodsInThread.Peek().AddInnerMethod(method);
+            }
+            else
+            {
+                TracedMethods.Add(method);
+            }
+            MethodsInThread.Push(method);
+        }
+
+        internal void StartTrace(MethodTracer method)
+        {
+            AddMethod(method);
+            method.StartTrace();
+        }
+
+        internal void StopTrace()
+        {
+            if (MethodsInThread.Count == 0)
+            {
+                throw new InvalidOperationException("Can't stop tracing method that doesn't exist");
+            }
+            MethodTracer popedMethod = MethodsInThread.Pop();
+            popedMethod.StopTrace();
+        }
+
+        internal ThreadTracer(int id)
+        {
+            threadId = id;
+        }
+
+
+    }
+}
