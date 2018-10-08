8adda5a8414df36f8ee3c120b0d740c0c4ab6a24
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-08 17:10:42 +0300

    implemented FileWriter

--- Tracer/Tracer/FileWriter.cs
+++ Tracer/Tracer/FileWriter.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Tracer
 {
-    class FileWriter
+    public class FileWriter : IWriter
     {
+        private string fileName;
+        public FileWriter(string fileN)
+        {
+            fileName = fileN;
+        }
+        public void Write(TraceResult traceResult, ISerializer serializer)
+        {
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Append))
+            {
+                serializer.SerializeResult(traceResult, fileStream);
+            }
+        }
+
+        public string FileName
+        {
+            get
+            {
+                return fileName;
+            }
+            set
+            {
+                fileName = value;
+            }
+        }
     }
 }
