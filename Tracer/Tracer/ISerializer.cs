1ed8b6ce02368c80541592652c61ae7e7b213a8a
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-08 16:56:06 +0300

    added ISerializer

---
+++ Tracer/Tracer/ISerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public interface ISerializer
+    {
+        void SerializeResult(TraceResult traceResult, Stream stream);
+    }
+}
