e01290abe1bd0cea24e31d0ec2c2bd60616acb08
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-08 17:12:32 +0300

    implemented ConsoleWriter

--- Tracer/Tracer/ConsoleWriter.cs
+++ Tracer/Tracer/ConsoleWriter.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Tracer
 {
-    class ConsoleWriter
+    public class ConsoleWriter : IWriter
     {
+        public void Write(TraceResult traceResult, ISerializer serializer)
+        {
+            using (Stream consoleOutPutStream = Console.OpenStandardOutput())
+            {
+                serializer.SerializeResult(traceResult, consoleOutPutStream);
+            }
+        }
     }
 }
