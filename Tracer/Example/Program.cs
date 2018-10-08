5c61f6f4086dd8ac1036220b55c2a3420eba843d
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-08 17:18:52 +0300

    adedd Example class

---
+++ Tracer/Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Tracer;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
