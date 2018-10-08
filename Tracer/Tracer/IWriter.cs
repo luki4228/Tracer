8f4ad04a6d980b938a54ac9cf72f3502ad0e53bc
Evgeniy Lushchik <zhenyalushik@gmail.com>
2018-10-08 17:06:17 +0300

    implemented IWriter

--- Tracer/Tracer/IWriter.cs
+++ Tracer/Tracer/IWriter.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Tracer
 {
-    interface IWriter
+    public interface IWriter
     {
+        void Write(TraceResult traceResult, ISerializer serializer);
     }
 }
